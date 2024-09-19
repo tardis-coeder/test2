bc98c8f95bef09b55b878a175a6e26ccb3441842
tardis-coeder <hov1n@naver.com>
2024-09-19 17:09:42 +0900

    Update Fruit.cs

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
-    public float timeAdd = 5;
+    public float timeAdd = 15;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
